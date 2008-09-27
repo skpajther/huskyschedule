a7f18e9256ca8c57d48d5684ea06d140db6ff169
danny.swisher <danny.swisher@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2008-09-27 23:47:44 +0000

    Updated the design and some function of the _header layout

--- huskyschedule/db/schema.rb
+++ huskyschedule/db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(:version => 20080925032735) do
     t.text     "notes"
     t.text     "credit_type"
     t.string   "section"
-    t.text     "schedule"
+    t.text     "times"
     t.text     "description"
     t.boolean  "crnc"
     t.integer  "parent_id",         :limit => 11
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(:version => 20080925032735) do
     t.boolean  "restrictions"
     t.text     "notes"
     t.string   "section"
-    t.text     "schedule"
+    t.text     "times"
     t.text     "description"
     t.boolean  "crnc"
     t.integer  "parent_id",         :limit => 11
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(:version => 20080925032735) do
     t.boolean  "restrictions"
     t.text     "notes"
     t.string   "section"
-    t.text     "schedule"
+    t.text     "times"
     t.text     "description"
     t.boolean  "crnc"
     t.integer  "parent_id",         :limit => 11
