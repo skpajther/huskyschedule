682ab8c161386b7314cceed9db7511d4c8dcb500
healy44 <healy44@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2008-10-04 08:04:07 +0000

    fixed search

--- huskyschedule/app/controllers/buildings_controller.rb
+++ huskyschedule/app/controllers/buildings_controller.rb
@@ -1,10 +1,19 @@
 class BuildingsController < ApplicationController
   def map
     @buildings = Building.find(:all, :order => :name)
-    @searching = false
-    @selected
-      #$query = mysql_query("SELECT name,abbrev FROM hs_campus_buildings WHERE abbrev LIKE '%$text%' OR name LIKE '%$text%' ORDER BY abbrev",$connection);
-    
+    if(params[:search_text] != nil)
+      @searching = true
+      @search_results = Building.find_by_sql(
+          "SELECT * 
+           FROM buildings 
+           WHERE abbrev LIKE '%#{params[:search_text]}%' 
+           OR name LIKE '%#{params[:search_text]}%' 
+           ORDER BY abbrev"
+      )
+    else
+      @searching = false
+    end
+    @selected = params[:selected]
   end
 
   def index
