accf31bb2f158878a73da37138dd0c519eb87cdf
healy44 <healy44@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2009-02-02 05:00:08 +0000

    Added status constants, credit_type constants, and get quarter by string

--- huskyschedule/app/models/course.rb
+++ huskyschedule/app/models/course.rb
@@ -11,6 +11,45 @@ class Course < ActiveRecord::Base
   serialize :times
   serialize :credit_type
   
+  #status constants
+  STATUS_CLOSED = 0
+  STATUS_OPEN = 1
+  STATUS_UNKNOWN = 2
+
+  #credit type contants
+  CREDITTYPE_NW = 0
+  CREDITTYPE_QSR = 1
+  CREDITTYPE_W = 2
+  CREDITTYPE_IS = 3 #I&S
+  CREDITTYPE_J = 4
+  CREDITTYPE_UNKNOWN = 5
+  CREDITTYPE_NOTLISTED = 6
+  CREDITTYPE_D = 7
+  CREDITTYPE_H = 8
+  CREDITTYPE_S = 9
+  
+  def self.get_credit_types(string_representation)
+    elements = string_representation.split(",")
+    types = []
+    for element in elements
+      element = element.strip
+      if(element == "NW")
+        types.push(CREDITTYPE_NW)
+      elsif(element == "QSR")
+        types.push(CREDITTYPE_QSR)
+      elsif(element == "W")
+        types.push(CREDITTYPE_W)
+      elsif(element == "I&S")
+        types.push(CREDITTYPE_IS)
+      elsif(element == "J")
+        types.push(CREDITTYPE_J)
+      else
+        types.push(CREDITTYPE_UNKNOWN)
+      end
+    end
+    return types    
+  end  
+  
   def self.quarter_disp_name(quarter_id)
     if(quarter_id > 0 && quarter_id < 6)
       return QUARTER_DISPLAY_NAMES[quarter_id-1]
@@ -22,6 +61,5 @@ class Course < ActiveRecord::Base
   def name
     return "#{self.deptabriev} #{self.number}"
   end
-  
-  
+      
 end
