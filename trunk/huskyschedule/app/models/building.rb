95b5dd5ca4e06faad4290e3b6e41cb20a4734d9f
healy44 <healy44@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2009-02-11 04:00:18 +0000

    Added constants, a method

--- trunk/huskyschedule/app/models/building.rb
+++ trunk/huskyschedule/app/models/building.rb
@@ -2,4 +2,15 @@ class Building < ActiveRecord::Base
   
   has_many :courses
   
+  #Building constants
+  BUILDING_NOTFOUND = -1 #Regex failed
+ 
+  def self.get_building_id(abbrev)
+    building = Building.find_by_abbrev(abbrev)
+    if(building.nil?) #building not already in there
+      building = Building.create(:abbrev=>abbrev)
+    end
+    return building.id
+  end
+  
 end
