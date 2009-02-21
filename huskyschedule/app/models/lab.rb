a7a2ccbb8711efe5c4baa2088aad08ea699d749b
healy44 <healy44@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2009-02-21 06:55:09 +0000

    Courses by Building added

--- huskyschedule/app/models/lab.rb
+++ huskyschedule/app/models/lab.rb
@@ -5,6 +5,17 @@ class Lab < ActiveRecord::Base
   belongs_to :building
   
   Lab.partial_updates = false
-  serialize :times
+  serialize :rendezvous
+  
+  def self.find_by_building_quarter_year_day(building_id, quarter_id, year, day, overall_times)
+    labs = Lab.find_by_sql("SELECT * FROM labs WHERE buildings LIKE '%#{building_id}%'")
+    labs.delete_if{|lab| 
+      !(lab.course.quarter_id==quarter_id && 
+        lab.course.year==year && 
+        Rendezvous.relevant_rendezvous(lab.rendezvous, building_id, day, overall_times)
+       ) 
+    }
+    return labs
+  end
   
 end
