4db9a0e5df49610b515d5f303db79ee80c732415
healy44 <healy44@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2009-02-21 06:55:09 +0000

    Courses by Building added

--- trunk/huskyschedule/app/models/quiz_section.rb
+++ trunk/huskyschedule/app/models/quiz_section.rb
@@ -5,7 +5,17 @@ class QuizSection < ActiveRecord::Base
   belongs_to :building
   
   QuizSection.partial_updates = false
-  serialize :times
+  serialize :rendezvous
   
-  
+  def self.find_by_building_quarter_year_day(building_id, quarter_id, year, day, overall_times)
+    quiz_sections = QuizSection.find_by_sql("SELECT * FROM quiz_sections WHERE buildings LIKE '%#{building_id}%'")
+    quiz_sections.delete_if{|quiz_section| 
+      !(quiz_section.course.quarter_id==quarter_id && 
+        quiz_section.course.year==year && 
+        Rendezvous.relevant_rendezvous(quiz_section.rendezvous, building_id, day, overall_times)
+       ) 
+    }
+    return quiz_sections
+  end
+
 end
