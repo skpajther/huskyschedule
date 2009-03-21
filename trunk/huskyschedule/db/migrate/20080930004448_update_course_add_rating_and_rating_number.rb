670d23ed72564cd08318e2d0c7b9bf0ae5dbfc56
danny.swisher <danny.swisher@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2009-03-21 18:48:18 +0000

    fixed migration so that total_reviews was switched to total_ratings

--- trunk/huskyschedule/db/migrate/20080930004448_update_course_add_rating_and_rating_number.rb
+++ trunk/huskyschedule/db/migrate/20080930004448_update_course_add_rating_and_rating_number.rb
@@ -1,11 +1,11 @@
 class UpdateCourseAddRatingAndRatingNumber < ActiveRecord::Migration
   def self.up
     add_column :courses, :rating, :integer
-    add_column :courses, :total_reviews, :integer
+    add_column :courses, :total_ratingss, :integer
   end
 
   def self.down
     remove_column :courses, :rating
-    remove_column :courses, :total_reviews
+    remove_column :courses, :total_ratings
   end
 end
