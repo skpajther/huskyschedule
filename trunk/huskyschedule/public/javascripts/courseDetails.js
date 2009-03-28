a1bfc7bb35cb629e555daaa1d60302ee9830b291
danny.swisher <danny.swisher@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2009-03-28 00:33:50 +0000

    fixed tabs in courses pae so that they are more flexible and can be used on the teachers page.

--- trunk/huskyschedule/public/javascripts/courseDetails.js
+++ trunk/huskyschedule/public/javascripts/courseDetails.js
@@ -1,69 +1,37 @@
-var tabbedWindowDiv;
-var overviewDiv;
-var classReviewsDiv;
-var teacherReviewsDiv;
-var mapDiv;
-var currentState;
+
+var Curr_Id;
+var tab_hash;
 
 function initCD() {
 	initializeCDfields();
 }
 
-function initializeCDfields() {
-	tabbedWindowDiv = document.getElementById("tabbedwindow");
-	overviewDiv = document.getElementById("overview");
-   	classReviewsDiv = document.getElementById("classReviews");
-   	tabbedWindowDiv.removeChild(classReviewsDiv);
-   	teacherReviewsDiv = document.getElementById("teacherReviews");
-   	tabbedWindowDiv.removeChild(teacherReviewsDiv);
-   	mapDiv = document.getElementById("mediumMapArea");
-	tabbedWindowDiv.removeChild(mapDiv);
-	currentState = 0;
+function initializeCDfields(initial_tab_id) {
+	tab_hash = new Hash();
+	Curr_Id = initial_tab_id;
 }
 
-function showTab(tabNumber) {
-	if(currentState != tabNumber) {
-		switch(currentState) {
-			case 0: //overview
-				tabbedWindowDiv.removeChild(overviewDiv);
-				document.getElementById("overviewtab").className = "normtab";
-				break;
-			case 1: //class reviews
-				tabbedWindowDiv.removeChild(classReviewsDiv);
-				document.getElementById("classreviewstab").className = "normtab";
-				break;
-			case 2: //teacher reviews
-				tabbedWindowDiv.removeChild(teacherReviewsDiv);
-				document.getElementById("teacherreviewstab").className = "normtab";
-				break;
-			case 3: //map
-				tabbedWindowDiv.removeChild(mapDiv);
-				document.getElementById("maptab").className = "normtab";
-				break;
-			default:
-				break;
-		}
-		switch(tabNumber) {
-			case 0: //overview
-				tabbedWindowDiv.appendChild(overviewDiv);
-				document.getElementById("overviewtab").className = "tabme";
-				break;
-			case 1: //class reviews
-				tabbedWindowDiv.appendChild(classReviewsDiv);
-				document.getElementById("classreviewstab").className = "tabme";
-				break;
-			case 2: //teacher review
-				tabbedWindowDiv.appendChild(teacherReviewsDiv);
-				document.getElementById("teacherreviewstab").className = "tabme";
-				break;
-			case 3: //map
-				tabbedWindowDiv.appendChild(mapDiv);
-				document.getElementById("maptab").className = "tabme";
-				break;
-			default: //overview
-				showTab(0);
-				return;
-		}	
-		currentState = tabNumber;
+function hideTab(id){
+	to_remove = document.getElementById(id);
+	tab_label = document.getElementById(id+"_tab");
+	parent = to_remove.parentNode
+	
+	tab_hash.set(id, to_remove);
+	
+	parent.removeChild(to_remove);
+	tab_label.className = "normtab";
+	return parent;
+}
+
+function showTab(new_id) {
+	if(Curr_Id != new_id) {
+		parent_div = hideTab(Curr_Id);
+		
+		new_content = tab_hash.get(new_id);
+		new_tab = document.getElementById(new_id+"_tab");
+		parent_div.appendChild(new_content);
+		new_tab.className = "tabme";
+		
+		Curr_Id = new_id;
 	}
 }
