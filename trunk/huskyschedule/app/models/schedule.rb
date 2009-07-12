b1c1c376ced2f271245aa45a49f31a83c6888a88
danny.swisher <danny.swisher@382b7068-8acd-11dd-a14b-9ba93c563a2f>
2009-07-12 01:08:25 +0000

    more schedule stuff

--- trunk/huskyschedule/app/models/schedule.rb
+++ trunk/huskyschedule/app/models/schedule.rb
@@ -4,6 +4,48 @@ class Schedule < ActiveRecord::Base
   
   Schedule.partial_updates = false
   serialize :courses
+  serialize :colors
+  serialize :quiz_sections
+  serialize :labs
+  
+  COLORS = {1=>"ff6666",   2=>"ff66cc",  3=>"cc66ff",  4=>"6666ff",  5=>"66ccff",  6=>"66ffcc",  7=>"66ff66",  8=>"ccff66",  9=>"ffcc66", 10=>"ff6666",
+            11=>"cc6666", 12=>"cc66cc", 13=>"6666cc", 14=>"66cccc", 15=>"66cc66", 16=>"cccc66", 17=>"cc6666", 18=>"666666", 19=>"9999cc", 20=>"ffcccc",
+            21=>"cc0000", 22=>"336600", 23=>"003399", 24=>"cccccc", 25=>"cc9966", 26=>"99cc66", 27=>"66cc99", 28=>"6699cc", 29=>"9966cc", 30=>"cc6699",
+            31=>"cccc66", 32=>"ff9966", 33=>"ffff66", 34=>"99ff66", 35=>"66ff99", 36=>"66ffff", 37=>"6699ff", 38=>"9966ff", 39=>"ff66ff", 40=>"ff6699"}
+  
+  def self.get_next_color(unavailable_colors)
+    if(unavailable_colors==nil || unavailable_colors.size==0)
+      return 1
+    else
+      max_val = unavailable_colors.sort.pop
+      if(max_val < 40)
+        return max_val+1
+      else
+        keys = COLORS.keys
+        found = nil
+        i= 0
+        while(i<keys.size && found==nil)
+          if(!unavailable_colors.include?(keys[i]))
+            found = keys[i]
+          end
+          i = i + 1
+        end
+        if(found!=nil)
+          return found
+        else
+          i=2
+          while(found==nil)
+            available = (keys*i) - unavailable_colors
+            if(available!=nil && available.size>0)
+              found = available[0]
+            end
+            i = i + 1
+          end
+          return found
+        end
+      end
+    end
+  end
   
   def self.authorized_to_view(schedule, curr_user)
     if(schedule.user_id==nil)
@@ -27,6 +69,9 @@ class Schedule < ActiveRecord::Base
       Schedule.create(schedule, {:schedule=>attributes}, curr_user)
       schedule.name = "#{curr_user.login}'s grab bag"
       schedule.courses = []
+      schedule.colors = []
+      schedule.quiz_sections = {}
+      schedule.labs = {}
       schedule.save!
     end
     return schedule
@@ -39,7 +84,23 @@ class Schedule < ActiveRecord::Base
     return []
   end
   
-  def self.add_to_schedule(schedule, course, curr_user)
+  def get_color_id_for_course(course)
+    if(course!=nil)
+      return self.colors[self.courses.index(course.id)]
+    else
+      return nil
+    end
+  end
+  
+  def get_color_for_course(course)
+    if(course!=nil)
+      return COLORS[get_color_id_for_course(course)]
+    else
+      return nil
+    end
+  end
+  
+  def self.add_to_schedule(schedule, course, curr_user, quiz_section=nil, lab=nil)
     if(schedule==nil)
       raise ScheduleError.new("No Schedule Given To Add To")
     end
@@ -53,7 +114,31 @@ class Schedule < ActiveRecord::Base
       if(schedule.courses==nil)
         schedule.courses = []
       end
+      if(schedule.colors==nil)
+        schedule.colors = []
+      end
+      if(schedule.quiz_sections==nil)
+        schedule.quiz_sections = {}
+      end
+      if(schedule.labs==nil)
+        schedule.labs = {}
+      end
       schedule.courses.push(course.id)
+      if(schedule.grab_bag)
+        while(schedule.colors.size < schedule.courses.size)
+          schedule.colors.push(get_next_color(schedule.colors))
+        end
+      end
+      if(quiz_section!=nil)
+        schedule.quiz_sections[course.id] = quiz_section.id
+      elsif(course.quiz_sections!=nil && course.quiz_sections.size>0)
+        schedule.quiz_sections[course.id] = course.quiz_sections[0]
+      end
+      if(lab!=nil)
+        schedule.labs[course.id] = lab.id
+      elsif(course.labs!=nil && course.labs.size>0)
+        schedule.labs[course.id] = course.labs[0]
+      end
       if(schedule.save)
         return "Course Added Successfully"
       else
